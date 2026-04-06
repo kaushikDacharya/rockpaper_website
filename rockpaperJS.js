685e80576d317cee6809166a8e1c4e923399b900
kaushikDacharya <acharyakaushik08@gmail.com>
2026-04-06 21:18:12 +0530

    Update rockpaperJS.js

--- rockpaperJS.js
+++ rockpaperJS.js
@@ -37,7 +37,7 @@ window.onload = function (){
 
 async function senddata(){
     try{
-const response = await fetch("https://rockpaper-website-1.onrender.com",{
+const response = await fetch("https://rockpaper-website-1.onrender.com/compute",{
     method : "POST",
     headers : {"Content-Type" : "application/json"},
     body : JSON.stringify({playerhand,computerhand})
