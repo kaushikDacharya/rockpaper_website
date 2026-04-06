466af373848d649c8f62213b936fe525dce37e07
kaushikDacharya <acharyakaushik08@gmail.com>
2026-04-06 19:59:46 +0530

    Update rockpaperPY.py

--- rockpaperPY.py
+++ rockpaperPY.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import numpy as np
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -29,4 +30,5 @@ def result():
         return jsonify({"result": "lose"})
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
